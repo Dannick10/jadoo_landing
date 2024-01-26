c904859d75fe69fb2a37497641ff6d75fd38ef15
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-26 14:56:04 -0300

    Email validity

---
+++ script.js
@@ -0,0 +1,71 @@
+const email = document.querySelector('#email')
+const notyferror = new Notyf({
+    duration: 1000,
+    position: {
+      x: 'center',
+      y: 'top',
+    },
+    types: [
+      {
+        type: 'warning',
+        background: 'orange',
+        icon: {
+          className: 'material-icons',
+          tagName: 'i',
+          text: 'warning'
+        }
+      },
+      {
+        type: 'error',
+        background: 'indianred',
+        duration: 2000,
+        dismissible: true
+      }
+    ]
+  });
+
+  const notywait = new Notyf({
+    duration: 1000,
+    position: {
+      x: 'center',
+      y: 'top',
+    },
+    types: [
+      {
+        type: 'warning',
+        background: 'orange',
+        icon: {
+          className: 'material-icons',
+          tagName: 'i',
+          text: 'warning'
+        }
+      },
+      {
+        type: 'error',
+        background: 'orange',
+        duration: 2000,
+        dismissible: true
+      }
+    ]
+  });
+
+
+
+document.querySelector('#btn_mall').addEventListener('click',(event)=>{
+  
+
+    if(email.validity.valueMissing){
+        msg = 'Please fill out the form'
+        notyferror.error('Please fill out the form');
+    }  else if(email.validity.typeMismatch){
+
+        notywait.error('invalid format put @');
+
+    } else if(statsEmail.valid){
+
+        notyferror.sucess('thanks');
+    }
+   
+    
+    event.preventDefault()
+})
